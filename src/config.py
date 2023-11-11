797a07e5bbbd68033a69d6244c94c0553f2593ca
RKhamitov <one_RKhamitov@fortebank.com>
2023-11-12 03:17:04 +0600

    fix .env

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
+# load .env file
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config(BaseSettings):
     DATABASE_URL: PostgresDsn
